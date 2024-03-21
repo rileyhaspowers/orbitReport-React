99d613e80ffed3b48af8d72a3a8915c26056180d
rileyhaspowers <rileyhaspowers@example.com>
2024-03-21 17:04:36 -0500

    finished react

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,22 @@
+import satData from "./satData.jsx";
+
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+ 
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}>
+        All Orbits
+      </button>
     </div>
   );
-};
+    }  
 
 export default Buttons;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,13 +3,22 @@ const Table = ({ sat }) => {
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
+     {sat.map((satellite) =>(
+     <tr key={satellite.id}>
+        <td>{satellite.name}</td>
+        <td>{satellite.type}</td>
+        <td>{satellite.launchDate}</td>
+        <td>{satellite.operational ? 'Active' : 'Inactive'}</td>
      </tr>
+)
+)}
      </tbody>
    </table>
   );
